d6a06c3c6a8759e771aec41c53e7a22fedc22093
JuanDev29 <judorozcocl29@gmail.com>
2023-02-10 17:00:24 -0500

    config apollo client graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
+import { ProjecDetails } from './pages/ProjecDetails'
+import { Projects } from './pages/Projects'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
-    <div>App</div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Navigate to='/projects' /> }/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/projects/:id' element={<ProjecDetails/>}/>
+      </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
---
+++ client/src/pages/ProjecDetails.jsx
@@ -0,0 +1,5 @@
+export function ProjecDetails() {
+  return (
+    <div>ProjecDetails</div>
+  )
+}
