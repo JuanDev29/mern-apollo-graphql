e2f9a5e92991cdb3ff505486fd79fd665d392bd7
JuanDev29 <judorozcocl29@gmail.com>
2023-02-10 15:44:38 -0500

    config - delete and update / Projects - Tasks

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -7,6 +7,8 @@ export const resolvers = {
     hello: () => "Hello World",
     getProjects: async () => await Project.find(),
     getTasks: async () => await Task.find(),
+    getProject: async (_, { _id }) => await Project.findById(_id),
+    getTask: async (_, { _id }) => await Task.findById(_id)
   },
   Mutation: {
     createProject: async (_, { name, description }) => {
@@ -16,11 +18,23 @@ export const resolvers = {
       const savedProject = await newProject.save()
       return savedProject
     },
+    deleteProject: async (_, { _id }) => {
+      const deletedProject = await Project.findByIdAndDelete(_id)
+      if(!deletedProject) throw new Error("Project not found")
+      await Task.deleteMany({projectId: deletedProject._id})
+      return deletedProject
+    },
+    updateProject: async (_, { _id, name, description }) => {
+      const updatedAt = moment(Date().now).format('MMMM Do YYYY, h:mm:ss a')
+      const updatedProject = await Project.findByIdAndUpdate(
+        {_id}, {name, description, updatedAt}, {new: true}
+      )
+      if(!updatedProject) throw new Error("Project not found")
+      return updatedProject
+    },
     createTask: async (_, { title, projectId }) => {
-
       const projectFound = await Project.findById(projectId)
       if(!projectFound) throw new Error("ProjectId does not exists")
-
       const createdAt = moment(Date().now).format('MMMM Do YYYY, h:mm:ss a')
       const updatedAt = moment(Date().now).format('MMMM Do YYYY, h:mm:ss a')
       const newTask = new Task({title, projectId, createdAt, updatedAt})
@@ -28,5 +42,18 @@ export const resolvers = {
       return savedTask
 
     },
+    deleteTask: async (_, { _id }) => {
+      const deletedTask = await Task.findByIdAndDelete(_id)
+      if(!deletedTask) throw new Error("Task not found")
+      return deletedTask
+    },
+    updateTask: async (_, { _id, title, projectId }) => {
+      const updatedAt = moment(Date().now).format('MMMM Do YYYY, h:mm:ss a')
+      const updateTask = await Task.findByIdAndUpdate(
+        {_id}, {title, projectId, updatedAt}, {new: true}
+      )
+      if(!updateTask) throw new Error("Task not found")
+      return updateTask
+    }
   }
 }
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -5,11 +5,17 @@ export const typeDefs = gql`
     hello: String
     getProjects: [Project]
     getTasks: [Task]
+    getProject(_id: ID!): Project
+    getTask(_id: ID!): Task
   }
 
   type Mutation {
     createProject(name: String, description: String): Project
+    deleteProject(_id: ID!): Project
+    updateProject(_id: ID!, name: String!, description: String): Project
     createTask(title: String, projectId: ID): Task
+    deleteTask(_id: ID!): Task
+    updateTask(_id: ID!, title: String!, projectId: ID!): Task
   }
 
   type Project {
